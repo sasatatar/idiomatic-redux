cb496d04a1678c6dea7d09a44ffa1eb59ab49075
Sasa Tatar <sasa.tatar@gmail.com>
2017-02-05 16:54:41 +0100

    11. Normalizing the state shape

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,49 +1,52 @@
-const todo = (state={}, action) => {
+import { combineReducers } from 'redux';
+import todo from './todo';
+
+const byId = (state={}, action) => {
     switch(action.type){
+        case 'TOGGLE_TODO':
         case 'ADD_TODO':
             return {
-                id: action.id,
-                text: action.text,
-                completed: false
-            }
-        case 'TOGGLE_TODO':
-            if(state.id !== action.id){
-                return state
-            }
-            return {
                 ...state,
-                completed: !state.completed
-            }
+                [action.id]: todo(state[action.id], action),
+            };
         default:
             return state
     }
 }
 
-const todos = (state=[], action) => {
-    switch(action.type){
+const allIds = (state = [], action) => {
+    switch (action.type){
         case 'ADD_TODO':
-            return [
-                ...state,
-                todo(undefined, action)
-            ]
-        case 'TOGGLE_TODO':
-            return state.map(t => todo(t, action))
+            return [...state, action.id];
         default:
-            return state
+            return state;
     }
-}
+};
+
+
+const todos = combineReducers({
+    byId,
+    allIds,
+})
 
 export default todos
 
+
+// SELECTORS
+// private selector
+const getAllTodos = (state) => state.allIds.map(id => state.byId[id])
+
+// public selectors
 export const getVisibleTodos = (state, filter) => {
+    const allTodos = getAllTodos(state);
     switch(filter){
         case 'all':
-            return state
+            return allTodos
         case 'completed':
-            return state.filter(todo => todo.completed)
+            return allTodos.filter(todo => todo.completed)
         case 'active':
-            return state.filter(todo => !todo.completed)
+            return allTodos.filter(todo => !todo.completed)
         default:
-            return state
+            return allTodos
     }
 }
